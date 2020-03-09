f26c7f16aa8c763ac5a499c6d6cb2d299a0807ad
Tom H Anderson <tom.h.anderson@gmail.com>
2020-03-08 20:01:20 -0600

    Removed unused config from testing

--- test/DataFixture/module/Db/config/module.config.php
+++ test/DataFixture/module/Db/config/module.config.php
@@ -7,44 +7,6 @@ namespace Db;
 use Doctrine\ORM\Mapping\Driver\XmlDriver;
 
 return [
-    'zf-doctrine-querybuilder-filter-orm' => [
-        'invokables' => [
-            'eq'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\Equals',
-            'neq'       => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\NotEquals',
-            'lt'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\LessThan',
-            'lte'       => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\LessThanOrEquals',
-            'gt'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\GreaterThan',
-            'gte'       => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\GreaterThanOrEquals',
-            'isnull'    => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\IsNull',
-            'isnotnull' => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\IsNotNull',
-            'in'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\In',
-            'notin'     => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\NotIn',
-            'between'   => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\Between',
-            'like'      => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\Like',
-            'notlike'   => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\NotLike',
-            'orx'       => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\OrX',
-            'andx'      => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ORM\AndX',
-        ],
-    ],
-
-    'zf-doctrine-querybuilder-filter-odm' => [
-        'invokables' => [
-            'eq'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\Equals',
-            'neq'       => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\NotEquals',
-            'lt'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\LessThan',
-            'lte'       => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\LessThanOrEquals',
-            'gt'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\GreaterThan',
-            'gte'       => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\GreaterThanOrEquals',
-            'isnull'    => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\IsNull',
-            'isnotnull' => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\IsNotNull',
-            'in'        => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\In',
-            'notin'     => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\NotIn',
-            'between'   => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\Between',
-            'like'      => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\Like',
-            'regex'     => 'Laminas\ApiTools\Doctrine\QueryBuilder\Filter\ODM\Regex',
-        ],
-    ],
-
     'doctrine' => [
         'driver' => [
             'db_driver'   => [
@@ -56,34 +18,6 @@ return [
                     __NAMESPACE__ . '\Entity' => 'db_driver',
                 ],
             ],
-        ],
-    ],
-
-    'view_manager' => [
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'json_exceptions'          => [
-            'display'    => true,
-            'ajax_only'  => true,
-            'show_trace' => true,
-        ],
-
-        'doctype'             => 'HTML5',
-        'not_found_template'  => 'error/404',
-        'exception_template'  => 'error/index',
-        'template_map'        => [
-            'layout/layout'           => __DIR__
-                                         . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__
-                                         . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ],
-        'template_path_stack' => [
-            __DIR__ . '/../view',
-        ],
-        'strategies'          => [
-            'ViewJsonStrategy',
         ],
     ],
 ];
